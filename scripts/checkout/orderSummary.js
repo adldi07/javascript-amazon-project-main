16af8acee5721566a1dff6a483449959f6fdef11
Adesh Kumar <adesh8061@gmail.com>
2024-11-03 02:50:12 +0530

    Create automated test for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -26,7 +26,7 @@ export function renderOrderSummary(){
 
         cartSummaryHTML+=`
         <div class="js-cart-item-container-${matchingProduct.id} 
-        cart-item-container">
+        cart-item-container js-cart-item-container">
             <div class="delivery-date">
                 Delivery date: ${dateString}
             </div>
@@ -42,14 +42,16 @@ export function renderOrderSummary(){
                 <div class="product-price">
                     $${formatCurrency(matchingProduct.priceCents)}
                 </div>
-                <div class="product-quantity">
+                <div class="product-quantity 
+                js-product-quantity-${matchingProduct.id} ">
                     <span>
                     Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
                     <span class="update-quantity-link link-primary">
                     Update
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-quantity-link"
+                    <span class="delete-quantity-link link-primary js-delete-quantity-link
+                    js-delete-link-${matchingProduct.id}"
                     data-product-id="${matchingProduct.id}">
                     Delete
                     </span>
@@ -106,7 +108,7 @@ export function renderOrderSummary(){
         return html ;
     }
 
-    document.querySelector('.js-order-summary').innerHTML= cartSummaryHTML;
+    document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
     document.querySelectorAll('.js-delete-quantity-link')
         .forEach((link)=>{
